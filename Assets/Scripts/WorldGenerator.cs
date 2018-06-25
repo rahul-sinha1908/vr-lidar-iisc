cdbc8992ba7684537ab59e98446c8e0ae745c634
Rahul Sinha <rahul.sinha1908@gmail.com>
2018-06-25 14:13:01 +0530

    VR enabled

--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -6,17 +6,30 @@ public class WorldGenerator : MonoBehaviour {
 
 	public GameObject objPrefab;
 	TextAsset asset;
-	string[] lines;
+	
+    List<string[]> lineSet;
+    List<GameObject> myObjs;
+    int curInd = 1;
 	// Use this for initialization
 	void Start () {
-		asset = Resources.Load("data") as TextAsset;
-		lines = asset.text.Split('\n');
-		Debug.Log("Total lines : "+lines.Length);
+
+        lineSet = new List<string[]>();
+        myObjs = new List<GameObject>();
+
+        for(int i = 1; i <= 15; i++)
+        {
+            asset = Resources.Load("VelodyneData/data ("+i+")") as TextAsset;
+            string[] lines = asset.text.Split('\n');
+            lineSet.Add(lines);
+            Debug.Log("Total lines : " + lines.Length);
+        }
+        Debug.Log("All Loaded");
 
 		spawnSceene();
+        StartCoroutine(playScene());
 	}
 	private void spawnSceene(){
-		foreach(string line in lines){
+		foreach(string line in lineSet[0]){
 			string[] xyz=line.Split(',');
 			if(xyz.Length>=3){
 				float x, y, z;
@@ -24,12 +37,47 @@ public class WorldGenerator : MonoBehaviour {
 				if(!float.TryParse(xyz[1], out y))continue;
 				if(!float.TryParse(xyz[2], out z))continue;
 
-				GameObject.Instantiate(objPrefab,new Vector3(x,y,z), Quaternion.identity);
+				GameObject obj = GameObject.Instantiate(objPrefab,new Vector3(x,y,z), Quaternion.identity);
+                myObjs.Add(obj);
 			}
 		}
 	}
+    private void reloadScene(int pos)
+    {
+        int i = 0;
+        foreach (string line in lineSet[pos])
+        {
+            string[] xyz = line.Split(',');
+            if (xyz.Length >= 3)
+            {
+                float x, y, z;
+                if (!float.TryParse(xyz[0], out x)) continue;
+                if (!float.TryParse(xyz[1], out y)) continue;
+                if (!float.TryParse(xyz[2], out z)) continue;
+
+                if (i < myObjs.Count)
+                {
+                    myObjs[i].transform.position = new Vector3(x, y, z);
+                    i++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
 	// Update is called once per frame
 	void Update () {
-		
+        
 	}
+
+    private IEnumerator playScene()
+    {
+        while (curInd < lineSet.Count)
+        {
+            yield return new WaitForSeconds(0.2f);
+            reloadScene(curInd++);
+        }
+    }
 }
