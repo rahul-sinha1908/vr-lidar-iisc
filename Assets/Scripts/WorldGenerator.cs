ec91369daad895d8484fba189a39d3fe8dd6dfe3
Rahul Sinha <rahul.sinha1908@gmail.com>
2018-07-13 11:48:23 +0530

    Loading all data in memory

--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -27,6 +27,7 @@ public class WorldGenerator : MonoBehaviour {
 	public Transform parentObj;
 	TextAsset asset;
 	string[] lines;
+    List<string[]> linesArray;
 	Vector3[] vertices;
     bool firstLoad = true;
 
@@ -35,6 +36,9 @@ public class WorldGenerator : MonoBehaviour {
 		GetComponent<MeshFilter>().mesh = mesh = new Mesh();
 		mesh.name = "Procedural Grid";
         myCapsules = new List<GameObject>();
+
+        linesArray=new List<string[]>();
+
         StartCoroutine(loadScenes());
 	}
 
@@ -57,24 +61,77 @@ public class WorldGenerator : MonoBehaviour {
             {
                 Debug.Log("It got in");
                 lines = asset.text.Split('\n');
+                linesArray.Add(lines);
                 Debug.Log("Total lines : " + lines.Length);
-                spawnScene();
+                //spawnScene();
                 i++;
                 actualFile = fileName + " (" + i + ")";
                 asset = Resources.Load(actualFile) as TextAsset;
                 Debug.Log("Loaded : " + actualFile + " : " + (asset != null));
-                yield return new WaitForSeconds(0.1f);
+                yield return new WaitForEndOfFrame();
             }
+            Debug.Log("Loading Done");
+
+            int ind=0;
+            while(spawnSceneInSequence(ind++)){
+                yield return new WaitForEndOfFrame();
+            }
+            Debug.Log("Display Done");
         }
     }
     // Update is called once per frame
     void Update()
     {
-        
-
         //loadNextScene();
     }
 
+    private bool spawnSceneInSequence(int ind){
+        if(ind>linesArray.Count)
+            return false;
+        
+		int totColumns=0;
+		int totRows=0;
+
+		float x, y, z;
+		float  temp1, temp2;
+		int  laserId, azimuth;
+		int previousLID=100;
+
+		List<LoadData> myList=new List<LoadData>();
+		foreach(string line in linesArray[ind]){
+			string[] xyz=line.Split(',');
+			if(xyz.Length>=3){
+
+				if(!float.TryParse(xyz[0], out x))continue;
+				if(!float.TryParse(xyz[1], out y))continue;
+				if(!float.TryParse(xyz[2], out z))continue;
+				if(!float.TryParse(xyz[7], out temp1))continue;
+				if(!float.TryParse(xyz[8], out temp2))continue;
+
+				laserId=Mathf.RoundToInt(temp1);
+				azimuth=Mathf.RoundToInt(temp2);
+
+				if(previousLID>laserId)
+					totColumns++;
+				if(totRows<laserId)
+					totRows=laserId;
+				
+				previousLID=laserId;
+				// laser - 7, verticalangle - 12
+				myList.Add(new LoadData(x,y,z,laserId,totColumns-1));
+				// GameObject obj = GameObject.Instantiate(objPrefab,new Vector3(x,y,z), Quaternion.identity, parentObj);
+				// obj.name="Cube : "+laserId+" - "+azimuth;
+			}
+		}
+		Debug.Log("Total Rows : "+totColumns+" : "+totRows);
+
+		initTheVertices(myList, totColumns, totRows);
+
+        if(firstLoad)
+            firstLoad = false;
+        
+        return true;
+    }
 
 
     private void spawnScene(){
