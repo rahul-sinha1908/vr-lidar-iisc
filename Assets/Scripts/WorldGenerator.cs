f2bff4b1de1bf8bd8926f422ea505c68e7ede352
Rahul Sinha <rahul.sinha1908@gmail.com>
2018-07-23 15:43:12 +0530

    Drawing and rendering cubes in form of mesh.

--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -7,6 +7,7 @@ public class WorldGenerator : MonoBehaviour {
 	private Mesh mesh;
 	private int xSize=-1, ySize=-1;
 	public float clearingDistance=0;
+	public float scale=0.1f;
 	private bool[] flag;
 	class LoadData{
 		public float x, y, z;
@@ -18,6 +19,9 @@ public class WorldGenerator : MonoBehaviour {
 			this.z=z;
 			this.laserId= laserId;
 			this.azimuth=azimuth;
+		}
+		public Vector3 point(){
+			return new Vector3(x,y,z);
 		}
 	}
 	public GameObject objPrefab;
@@ -79,80 +83,55 @@ public class WorldGenerator : MonoBehaviour {
 	void initTheVertices(List<LoadData> myList, int width, int height){
 		xSize=width-1;
 		ySize=height-1;
-		vertices = new Vector3[(width) * (height)];
-		flag = new bool[(width) * (height)];
-		for(int i=0;i<flag.Length;i++)
-			flag[i]=false;
-		foreach(LoadData data in myList){
-			int y = (data.laserId%2==0)?data.laserId/2-1:15+data.laserId/2;
-			// int y = data.laserId;
-			int x = data.azimuth;
-			int index=y*width+x;
-			//Debug.Log("Index : "+)
-			if(index>=0 && index<vertices.Length){
-				//Debug.Log("Index : "+)
-				flag[index]=true;
-				vertices[index]=new Vector3(data.x, data.y, data.z);
-				if(isDrawCubes){
-					GameObject obj = GameObject.Instantiate(objPrefab,new Vector3(data.x,data.y,data.z), Quaternion.identity, parentObj);
-					obj.name="Cube ("+index+"): "+data.laserId+"-"+y+" - "+x;
+
+		//Get Some important Points
+		GameObject obj = GameObject.Instantiate(objPrefab, Vector3.zero, Quaternion.identity);
+		Mesh objMesh = obj.GetComponent<MeshFilter>().mesh;
+		int count = objMesh.vertices.Length;
+		int triCount = objMesh.triangles.Length;
+
+		Vector3[] meshVertices=new Vector3[objMesh.vertices.Length];
+		objMesh.vertices.CopyTo(meshVertices, 0);
+		
+		int[] meshTriangles=new int[objMesh.triangles.Length];
+		objMesh.triangles.CopyTo(meshTriangles, 0);
+		Destroy(obj);
+		//Done getting{
+
+		int vertexLimit=5400;
+		Debug.Log("Total Planes : "+(myList.Count/vertexLimit));
+		for(int startPoint=0; startPoint<myList.Count; startPoint+=vertexLimit/2){
+			obj = GameObject.Instantiate(objPrefab, Vector3.zero, Quaternion.identity, transform);
+			obj.transform.localScale=new Vector3(1,1,1);
+			objMesh = obj.GetComponent<MeshFilter>().mesh = new Mesh();
+			objMesh.name="Plane";
+
+			vertices = new Vector3[vertexLimit*count];
+			int[] triangles = new int[vertexLimit*triCount];
+			// int[] triangles;
+			// List<int> triList=new List<int>();
+
+			
+			for(int i=startPoint, si=0;i<myList.Count && i<startPoint+vertexLimit;i++, si++){
+				for(int j=0;j<count;j++){
+					vertices[si*count+j]=myList[i].point()+meshVertices[j]*scale;
+				}
+				for(int j=0;j<triCount;j++){
+					triangles[si*triCount+j]=(si*count)+meshTriangles[j];
+					// triList.Add((i*count)+meshTriangles[j]);
 				}
 			}
+			//drawCubes();
+			
+			objMesh.vertices=vertices;
+			objMesh.triangles=triangles;
+			// mesh.triangles=triList.ToArray();
+			objMesh.RecalculateNormals();
 		}
 
-		for (int i = 0; i < vertices.Length; i++)
-			if(vertices[i]!=null){
-				vertices[0]=vertices[i];
-				break;
-			}
-		for (int i = 1; i < vertices.Length; i++) {
-			if(vertices[i]==null){
-				vertices[i]=vertices[i-1];
-			}
-		}
-
-		// for (int x = 0; x <= xSize; x++) {
-		// 	for (int y = 0; y <= ySize; y++) {
-		// 		int i = y*width+x;
-		// 		if(y==0)
-		// 			continue;
-		// 		int pi = (y-1)*width+x;
-		// 		if(Vector3.SqrMagnitude(vertices[i]-vertices[pi])>clearingDistance)
-		// 			vertices[i]=vertices[pi];
-		// 	}
-		// }
-
-
-		// if(isDrawCubes)
-		// 	drawCubes();
-		mesh.vertices=vertices;
-		
-		int[] triangles = new int[xSize * ySize * 6];
-		for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++) {
-			for (int x = 0; x < xSize; x++, ti += 6, vi++) {
-				Vector3 v1=vertices[vi], v2= vertices[vi+1], v3=vertices[vi+xSize+1], v4=vertices[vi+xSize+2];
-				if(Vector3.SqrMagnitude(v1-v2)>clearingDistance
-					|| Vector3.SqrMagnitude(v2-v3)>clearingDistance
-					|| Vector3.SqrMagnitude(v3-v4)>clearingDistance
-					|| Vector3.SqrMagnitude(v4-v1)>clearingDistance)
-					continue;
-				if(!flag[vi] || !flag[vi+xSize+1] || !flag[vi+1] || !flag[vi+xSize+2])
-					continue;
-				triangles[ti] = vi;
-				triangles[ti + 3] = triangles[ti + 2] = vi + xSize + 1;
-				triangles[ti + 4] = triangles[ti + 1] = vi + 1;
-				triangles[ti + 5] = vi + xSize + 2;
-				// if(Vector3.SqrMagnitude(vertices[i]-vertices[pi])>clearingDistance)
-				// 	vertices[i]=vertices[pi];
-			}
-		}
-		mesh.triangles=triangles;
-		mesh.RecalculateNormals();
-		
 		Debug.Log("Done Drawing"+vertices.Length);
 		//StartCoroutine(Generate());
 	}
-
 	private void drawCubes(){
 		if(vertices==null){
 			Debug.Log("Vertices null");
@@ -176,7 +155,7 @@ public class WorldGenerator : MonoBehaviour {
 		// Gizmos.color = Color.black;
 		// for (int i = 0; i < vertices.Length; i++) {
 		// 	//Gizmos.DrawSphere(vertices[i], 0.1f);
-		// 	Gizmos.DrawCube(vertices[i], new Vector3(1,1,1)*0.05f);
+		// 	Gizmos.DrawCube(vertices[i], new Vector3(1,1,1)*0.001f);
 		// }
 	}
 
